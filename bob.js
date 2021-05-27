4e0dbc0a06a8968da058413f5cb3a93e4fea7ed5
ShashwatNamdev <77872270+ShashwatNamdev@users.noreply.github.com>
2021-05-27 16:44:19 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,14 +1,12 @@
 class Bob{
     constructor(x,y,r){
         var options = {
-            'restitution':1
-            // 'friction':1.0,
-            // 'density':1.0
+            restitution:1
         }
         this.body = Bodies.circle(x, y, r, options);
         this.x = x;
         this.y = y;
-        this.radius = radius;
+        this.r = r;
         World.add(world, this.body);
     }
     display(){
@@ -17,7 +15,8 @@ class Bob{
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         ellipseMode(CENTER);
-        ellipse(this.ellipse, 0, 0, this.radius);
+        fill("pink");
+        ellipse(0, 0, this.radius);
         pop();
       }
 }
--- sketch.js
+++ sketch.js
@@ -2,28 +2,44 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-var Bob1;
-var Bob2;
-var Bob3;
-var Bob4;
-var Bob5;
+var bob1;
+var bob2;
+var bob3;
+var bob4;
+var bob5;
+var roof;
+var rope1;
+var rope2;
+var rope3;
+var rope4;
+var rope5;
 
-function preload()
-{
+function preload(){
 	
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(800, 600);
     
     
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-    Bob1 = new Bob(400,350,50);
-
+  roof = new Roof(300,200,250,20);
+  bob1 = new Bob(200,450,10);
+  bob2 = new Bob(250,450,10);
+  bob3 = new Bob(300,450,10);
+  bob4 = new Bob(350,450,10);
+  bob5 = new Bob(400,450,10);
+  rope1 = new Rope(bob1.body,{x:200,y:200});
+  rope2 = new Rope(bob2.body,{x:250,y:200});
+  rope3 = new Rope(bob3.body,{x:300,y:200});
+  rope4 = new Rope(bob4.body,{x:350,y:200});
+  rope5 = new Rope(bob5.body,{x:400,y:200});
+  
 	Engine.run(engine);
   
 }
@@ -31,9 +47,18 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
-  Bob1.display();
-  
+  background(245);
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  roof.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
   drawSprites();
  
 }
